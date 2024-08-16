a7590a2c968629f2a35609c4255c4ac3844ab3ae
welu28 <27luw@millburn.org>
2024-08-16 12:40:46 -0400

    Create snippets.java

---
+++ snippets/snippets.java
@@ -0,0 +1,36 @@
+package snippets;
+import java.util.*;
+
+public class snippets {
+    public static long modExp(long a, long b, long M){
+        long res = 1;
+        while(b > 0){
+            if((b & 1) == 1){
+                res = (res*a) % M;
+            }
+            b = b >> 1;
+            a = (a*a)%M;
+    
+        }
+        return res;
+    }
+
+    public static int[] readArr(Scanner io) {
+        int[] arr = Arrays.stream(io.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+        return arr;
+    }
+
+    public static int kadanes(int[] arr) {
+        int max_so_far = arr[0];
+        int max_ending_here = arr[0];
+
+        for(int i = 1; i < arr.length; i++) {
+            max_ending_here = Math.max(arr[i], max_ending_here + arr[i]);
+            max_so_far = Math.max(max_ending_here, max_so_far);
+        }
+
+        return max_so_far;
+    }
+
+    public static int gcd(int a, int b) { return b == 0 ? a : gcd(b, a % b); }
+}
